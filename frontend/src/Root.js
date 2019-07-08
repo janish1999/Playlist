bfd4ffb55cb4417755f1bbfe366ff5d095a25a20
Janish <JSjanish.1999@gmail.com>
2019-07-08 21:49:28 +0530

    Final

---
+++ frontend/src/Root.js
@@ -0,0 +1,66 @@
+import React from "react";
+import withRoot from "./withRoot";
+
+import {Query} from "react-apollo";
+import {gql} from 'apollo-boost';
+import App from './pages/App'
+
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Profile from "./pages/Profile";
+import Header from './components/Shared/Header'
+import Loading from "./components/Shared/Loading"
+import Error from './components/Shared/Error'
+
+export const UserContext = React.createContext();
+
+const Root = () => (
+    <Query query={ME_QUERY} fetchPolicy="cache-and-network">
+        {({data,loading,error})=>{
+            if (loading) return <Loading/>
+            if (error) return <Error error={error}/>
+
+            const currentUser = data.me;
+
+            return (
+                <Router>
+                    <UserContext.Provider value={currentUser}>
+                    <Header currentUser={currentUser}/>
+                    <Switch>
+                    <Route exact  path={"/"} component={App}/>
+                    <Route path={"/profile/:id"} components={Profile}/>
+                    </Switch>
+                    </UserContext.Provider>
+                </Router>
+            );
+        }
+        }
+    </Query>
+);
+
+export const ME_QUERY = gql`
+    {
+        me {
+            id
+            username
+            email 
+            likeSet{
+                track{
+                    id
+                }
+            }
+        }
+    }
+`;
+
+
+const GET_TRACKS_QUERY = gql`
+    {
+        tracks{
+            id
+            title
+            description
+            url
+            }
+    }
+`;
+export default withRoot(Root);
